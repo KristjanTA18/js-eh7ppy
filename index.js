2b560a8c446b4acad86d4fb39c014ed1e40b2af3
KristjanTA18 <kristjan.sepp@vikk.ee>
2020-11-30 15:21:00 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById('app'); // Select element with id "app"
+appDiv.innerHTML = `Hello World`; // Inject something into that element
+
+function reqListener(){
+  appDiv.innerHTML = JSON.parse(this.responseText)
+  .extract_html; //Inject something into that element
+  console.log(this.responseText)
+}
+
+function reqListener(){
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open(
+  "GET", 
+"https://en.wikipedia.org/api/rest_v1/page/summary/Viljandi_County");
+oReq.send();
